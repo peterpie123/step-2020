9e3cd061f90c8c197d93c58961696600a79c47a4
Peter Geertsema <peterjgeertsema@gmail.com>
2020-05-29 15:57:14 -0400

    Modularize code and add more constants

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -24,24 +24,41 @@ const RANDOM_FACT_OUTPUT_ID = 'fact-container';
 const USER_FAVORITE_INPUT_ID = 'agree-field';
 //ID of the div where favorite search results are put
 const FAVORITE_SEARCH_RESULT_ID = 'agree-container';
+//Message shown when the user's favorite is the same as one given here
+const FAVORITE_EXACT_MATCH = 'Exact match!';
+//Message shown when the user's favorite is not similar to any here
+const FAVORITE_NO_MATCH = 'No match here. Try another search or add this to your favorites!';
+//Message prompting users to click on one of the favorite links
+const FAVORITE_SEARCH_TIP = 'Tip: These are some of my favorites. Clicking on one'
+                             + ' will favorite it for you too!'
+//ID of the list containing all the search results from favorite search
+const FAVORITE_SEARCH_RESULT_LIST_ID = 'fav-search-list';
 //ID of the sidebar where user-selected favorites go
 const USER_FAVORITE_LIST_ID = 'fav-list';
 //ID of the placeholder text in the favorites are when there are no favorites selected
 const FAVORITE_PLACEHOLDER_ID = 'fav-placeholder';
-
+//Text in the favorite placeholder
+const FAVORITE_PLACEHOLDER_TEXT = `You haven't favorited any items yet!`;
+//Tag for creating a single checkbox in HTML
+const HTML_CHECKBOX_TAG = '<input type="checkbox"/>'
+//Property for what the user typed/selected in an input field
+const TEXT_SELECTION = 'value';
+//Property for whether a checkbox is checked or not
+const CHECKBOX_IS_CHECKED = 'checked';
 
 //dictionary of all the facts that can be pulled up
 const FACTS = {
-  movies: ['V for Vendetta', 'Monty Python and the Holy Grail', 
-           'Lord of the Rings (all of them)'],
+  movies: ['V for Vendetta', 'Monty Python and the Holy Grail',
+    'Lord of the Rings (all of them)'],
   shows: ['Seinfeld', 'Star Trek: DS9', 'The Mandalorian', 'Night on Earth'],
   food: ['Sushi', 'Pizza', 'Yogurt', 'Bacon', 'Lasagna', '50¢ Walmart Pies'],
   videoGames: ['Civ 5', 'Portal 2', 'Kerbal Space Program', 'Skyrim'],
 }
 //array of all the facts, pulled from above
-const FACTS_ARR = mergeLists();
+const FACTS_NAMES = mergeLists();
 //lowercase array of the facts
-const FACTS_ARR_LC = FACTS_ARR.map(v => v.toLowerCase());
+const FACTS_NAMES_LOWERCASE = FACTS_NAMES.map(v => v.toLowerCase());
+
 //store what the user favorites
 let userFavorites = new Set();
 
@@ -87,15 +104,44 @@ function findSubstrings(arr, substring) {
   return out;
 }
 
+//Appends a new HTML element to the given parent ID with the given information
+function appendElement(parentId, tagName, innerHtml, elementId = undefined, onclick = undefined) {
+  let tag = document.createElement(tagName);
+  let parent = document.getElementById(parentId);
+
+  tag.innerHTML = innerHtml;
+  if (elementId !== undefined) {
+    tag.id = elementId;
+  }
+  if(onclick !== undefined) {
+    tag.onclick = onclick;
+  }
+  parent.appendChild(tag);
+}
+
+//Removes the given element from the DOM
+function removeElement(elementId) {
+  document.getElementById(elementId).remove();
+}
+
+//removes all the children from the parent element
+function deleteChildren(parentId) {
+  document.getElementById(parentId).innerHTML = '';
+}
+
+//Retrieves the specified property from the given html element id
+function retrieveProperty(elementId, propertyName) {
+  return document.getElementById(elementId)[propertyName];
+}
+
 /* Put a random fact from the given list into the appropriate container */
 function generateFact() {
-  let dropdown = document.getElementById(FAVORITE_CATEGORY_ID);
-  let selection = dropdown.options[dropdown.selectedIndex].value;
+  let selection = retrieveProperty(FAVORITE_CATEGORY_ID, TEXT_SELECTION);
   let options;
 
   /* Check if we need to merge all lists */
   if (selection === MERGE_ALL_LISTS) {
-    options = FACTS_ARR;
+    options = FACTS_NAMES;
   } else {
     options = FACTS[selection];
   }
@@ -109,38 +155,36 @@ function generateFact() {
 /* Checks user input against all the facts above and shows options similar
  * what they typed. */
 function checkAgree() {
-  let textbox = document.getElementById(USER_FAVORITE_INPUT_ID);
-  let input = textbox.value;
-  //the value being put in the agree container
-  let output = '';
+  let input = retrieveProperty(USER_FAVORITE_INPUT_ID, TEXT_SELECTION);
+
+  //remove any current contents so we can insert the results of this search
+  deleteChildren(FAVORITE_SEARCH_RESULT_ID);
 
   //first check if input exactly matches a fact, ignoring case
-  if (FACTS_ARR_LC.includes(input.toLowerCase())) {
-    output = '<p>Exact match!</p>';
+  if (FACTS_NAMES_LOWERCASE.includes(input.toLowerCase())) {
+    appendElement(FAVORITE_SEARCH_RESULT_ID, 'p', FAVORITE_EXACT_MATCH);
   } else {
     //check if input is a substring of all the facts
-    let substrings = findSubstrings(FACTS_ARR, input);
+    let substrings = findSubstrings(FACTS_NAMES, input);
 
-    //Show the user each possibilty and attach a listener to auto-populate the textbox
+    //Show the user each possibilty
     if (substrings.length > 0) {
-      output += `<br/>Tip: These are some of my favorites. Clicking on one 
-                       will favorite it for you too!\n`
-      output += '<ul>\n'
+      //insert a line break before displaying results
+      appendElement(FAVORITE_SEARCH_RESULT_ID, 'br', '');
+      appendElement(FAVORITE_SEARCH_RESULT_ID, 'p', FAVORITE_SEARCH_TIP);
+
+      //create a list to hold the results and populate it
+      appendElement(FAVORITE_SEARCH_RESULT_ID, 'ul', '', FAVORITE_SEARCH_RESULT_LIST_ID);
 
       for (let i = 0; i < substrings.length; i++) {
-        //On click add the item to the users' favorite list
-        output += `<li onclick="addUserFavorite('${substrings[i]}')">
-                    ${substrings[i]}</li>\n`;
+        //call addUserFavorite when this element is clicked
+        appendElement(FAVORITE_SEARCH_RESULT_LIST_ID, 'li', substrings[i], /* id = */ undefined, 
+                      /* onclick = */ () => addUserFavorite(substrings[i]));
       }
-
-      output += '</ul>'
     } else {
-      output = 'No match here. Try another search or add this to your favorites!';
+      appendElement(FAVORITE_SEARCH_RESULT_ID, 'p', FAVORITE_NO_MATCH);
     }
   }
-
-  let container = document.getElementById(FAVORITE_SEARCH_RESULT_ID);
-  container.innerHTML = output;
 }
 
 /* Called when the user wants to add to their favorites */
@@ -155,15 +199,12 @@ function addUserFavorite(favorite) {
   if (!userFavorites.has(favorite)) {
     //if this is the first favorite, need to remove the placeholder
     if (userFavorites.size === 0) {
-      document.getElementById(FAVORITE_PLACEHOLDER_ID).remove();
+      removeElement(FAVORITE_PLACEHOLDER_ID);
     }
 
     userFavorites.add(favorite);
-
-    let labelTag = document.createElement('label');
-    labelTag.innerHTML = `<input type="checkbox"/>${favorite}`
-
-    document.getElementById(USER_FAVORITE_LIST_ID).appendChild(labelTag);
+    //append the element with a checkbox as well as the text
+    appendElement(USER_FAVORITE_LIST_ID, 'label', `${HTML_CHECKBOX_TAG}${favorite}`);
   }
 }
 
@@ -187,10 +228,6 @@ function removeUserFavorites() {
   //so instead check if there is any non-whitespace left
   if (list.innerHTML.trim() === '') {
     //add placeholder message
-    let listTag = document.createElement('p');
-
-    listTag.id = FAVORITE_PLACEHOLDER_ID;
-    listTag.innerText = `You haven't favorited any items yet!`;
-    list.appendChild(listTag);
+    appendElement(USER_FAVORITE_LIST_ID, 'p', FAVORITE_PLACEHOLDER_TEXT, FAVORITE_PLACEHOLDER_ID);
   }
 }
