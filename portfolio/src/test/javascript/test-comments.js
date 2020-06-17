2da2f806ce7dce0dd29d08ff4ae56b71e43e6f82
Peter Geertsema <peterjgeertsema@gmail.com>
2020-06-17 14:56:48 +0000

    Setup for JavaScript testing

---
+++ portfolio/src/test/javascript/test-comments.js
@@ -0,0 +1,29 @@
+import "@babel/polyfill";
+import chrome from "selenium-webdriver/chrome";
+import { Builder, By, Key, Capabilities } from "selenium-webdriver";
+import assert from "assert";
+import { path } from "chromedriver";
+let driver = null;
+const chromeOptions = new chrome.Options().headless();
+const URL = "https://www.google.com/webhp?hl=en";
+
+describe("Selenium", () => {
+  beforeEach(async () => {
+    driver = await new Builder(path)
+      .forBrowser("chrome")
+      .setChromeOptions(chromeOptions)
+      .build();
+    await driver.get(URL);
+  });
+
+  afterEach(async () => {
+    await driver.quit();
+  });
+
+  it("should render a message on a Google search result", async () => {
+    const element = await driver.findElement(By.name("q"));
+    await element.sendKeys("webdriver", Key.RETURN);
+    const res = await driver.findElement(By.css(".LC20lb")).getText();
+    assert.notEqual(res, null);
+  });
+});
