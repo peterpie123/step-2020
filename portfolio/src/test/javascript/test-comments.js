9b07c986a182e9ec79753a293b27d5dfaf343dc6
Peter Geertsema <peterjgeertsema@gmail.com>
2020-06-18 14:15:36 +0000

    Add front-end tests

--- portfolio/src/test/javascript/test-comments.js
+++ portfolio/src/test/javascript/test-comments.js
@@ -1,31 +1,72 @@
-import "@babel/polyfill";
-import chrome from "selenium-webdriver/chrome";
-import { Builder, By, Key, Capabilities } from "selenium-webdriver";
-import assert from "assert";
-import { path } from "chromedriver";
-let driver = null;
-const chromeOptions = new chrome.Options().headless();
-const URL = "https://www.google.com/webhp?hl=en";
+import '@babel/polyfill';
+import chrome from 'selenium-webdriver/chrome';
+import { Builder, By, Key, Capabilities } from 'selenium-webdriver';
+import assert, { doesNotMatch } from 'assert';
+import { path } from 'chromedriver';
+const fetch = require('node-fetch');
 
-describe("Selenium", () => {
-  beforeEach(async () => {
-    console.log("Before");
-    driver = await new Builder(path)
-      .forBrowser("chrome")
-      .setChromeOptions(chromeOptions)
-      .build();
+let driver = null;
+const URL = 'http://localhost:8081';
+const GET_COMMENTS = '/data?sort-ascending=true&pagination=0';
+const WAIT_TIME = 500;
+
+/** Gets comments from the server */
+async function getComments() {
+  return fetch(`${URL}${GET_COMMENTS}`).then(r => r.json());
+}
+
+describe('Selenium tests', () => {
+  before(async () => {
+    let options = new chrome.Options();
+
+    driver = await new Builder(path).forBrowser('chrome').setChromeOptions(options).build();
+    // Pull up the comments webpage
     await driver.get(URL);
   });
 
-  afterEach(async () => {
-    console.log("After");
-    await driver.quit();
+  describe('Comment management', () => {
+    let nameField, textField, submitButton;
+    let numComments;
+    before(async () => {
+      nameField = await driver.findElement(By.id('comment-name'));
+      textField = await driver.findElement(By.id('comment-text-input'));
+      submitButton = await driver.findElement(By.id('comment-submit'));
+      numComments = (await getComments()).length;
+    });
+
+    it('should create a comment with just a name and text', async () => {
+      await nameField.sendKeys('Test name');
+      await textField.sendKeys('Test comment');
+      await submitButton.click();
+
+      // Really not good practice, but there is a delay between posting a comment and it 
+      // appearing in the database
+      setTimeout(async () => {
+        let newNum = (await getComments()).length;
+        // There should be one more comment
+        assert.strictEqual(numComments + 1, newNum);
+        numComments = newNum;
+      }, WAIT_TIME);
+    });
+
+    it('should be able to delete a comment', async () => {
+      await nameField.sendKeys('Delete name');
+      await textField.sendKeys('Delete comment');
+      await submitButton.click();
+
+      setTimeout(async () => {
+        let commentToggle = await driver.findElement(By.className('comment-toggle'));
+        await commentToggle.click();
+        let deleteButton = await driver.findElement(By.id('comments-delete'));
+        await deleteButton.click();
+
+        // Should be same number after creating and deleting a comment
+        assert.strictEqual(numComments, (await getComments()).length)
+      }, WAIT_TIME);
+    });
   });
 
-  it("should render a message on a Google search result", async () => {
-    const element = await driver.findElement(By.name("q"));
-    await element.sendKeys("webdriver", Key.RETURN);
-    const res = await driver.findElement(By.css(".LC20lb")).getText();
-    assert.notEqual(res, null);
+  after(async () => {
+    await driver.quit();
   });
 });
